59c126bdce15a1e57025d229cd02697148771fa6
Sophia Cherkaoui <soph789opr@gmail.com>
2025-03-24 19:36:09 -0400

    Update app.py - more path changes

--- giftsite/app.py
+++ giftsite/app.py
@@ -26,13 +26,13 @@ def check_text():
 @app.route('/read', methods=['GET'])
 def read_text():
     try:
-        with open("data/results.txt", "r") as file:
+        with open("giftsite/data/results.txt", "r") as file:
             content = file.read()
         names = content.split()
         random_name = random.choice(names) if names else None
         name_to_remove = random_name
         updated_content = content.replace(name_to_remove, "")
-        with open("data/results.txt", "w") as file:
+        with open("giftsite/data/results.txt", "w") as file:
             file.write(updated_content)
         return jsonify({"content": content, "you_will_give_a_gift_to": random_name})
     except FileNotFoundError:
@@ -44,12 +44,12 @@ def remove_text():
     name_to_remove = data.get("text", "")
 
     try:
-        with open("data/results.txt", "r") as file:
+        with open("giftsite/data/results.txt", "r") as file:
             content = file.read()
 
         if name_to_remove in content:
             updated_content = content.replace(name_to_remove, "")
-            with open("data/results.txt", "w") as file:
+            with open("giftsite/data/results.txt", "w") as file:
                 file.write(updated_content)
             return jsonify({"message"})
         else:
